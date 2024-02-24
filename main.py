280fd536dbcfe82831d1d8ce9bbb38484ad3c7d0
Raphael Moragas Dias Taveira Gonçalves <raphael6384@gmail.com>
2024-02-24 19:46:01 +0000

    Logica
    lógica de movimentação implementada e correndo bem

--- main.py
+++ main.py
@@ -10,15 +10,17 @@ pygame.init()
 
 # Configurações da tela
 tam_grade = 9
-tam_cel = 60
-tam_tela = tam_cel * tam_grade
+tam_cel = 50
+larg_borda = tam_cel // 3
+tam_tela = tam_cel * tam_grade + larg_borda * 2
 tela = pygame.display.set_mode((tam_tela, tam_tela))
 pygame.display.set_caption('Jogo Cogito')
 
 # Cores
-cor_fundo = (30, 30, 30)
-cor_grade = (60, 60, 60)
-cor_peca = (200, 200, 50)
+cor_fundo = (0, 0, 0)
+cor_grade = (255, 255, 255)
+cor_peca = (255, 0, 0)
+cor_btn = (100, 100, 200)
 
 # Tabuleiro
 tab = [[0] * tam_grade for _ in range(tam_grade)]
@@ -33,9 +35,17 @@ def colocar_pecas():
 
 def desenhar():
     tela.fill(cor_fundo)
+    # Desenhar botões
+    for x in range(tam_grade):
+        pygame.draw.rect(tela, cor_btn, (x * tam_cel + larg_borda, 0, tam_cel, larg_borda))  # Top
+        pygame.draw.rect(tela, cor_btn, (x * tam_cel + larg_borda, tam_tela - larg_borda, tam_cel, larg_borda))
+    for y in range(tam_grade):
+        pygame.draw.rect(tela, cor_btn, (0, y * tam_cel + larg_borda, larg_borda, tam_cel))  # Left
+        pygame.draw.rect(tela, cor_btn, (tam_tela - larg_borda, y * tam_cel + larg_borda, larg_borda, tam_cel))
+
     for y in range(tam_grade):
         for x in range(tam_grade):
-            rect = pygame.Rect(x * tam_cel, y * tam_cel, tam_cel, tam_cel)
+            rect = pygame.Rect(x * tam_cel + larg_borda, y * tam_cel + larg_borda, tam_cel, tam_cel)
             pygame.draw.rect(tela, cor_grade, rect, 1)
             if tab[y][x] == 1:
                 pygame.draw.rect(tela, cor_peca, rect)
@@ -60,18 +70,24 @@ def mover_coluna(indice, direcao):
         tab[i][indice] = coluna[i]
 
 def processa_clique(x, y):
-    if x < tam_cel:  # Botão esquerdo para linhas
+    if (x < larg_borda and y < larg_borda) or (x > tam_tela - larg_borda and y > tam_tela - larg_borda): # Exclusão de quinas
+        pass
+    elif x < larg_borda:  # Botão esquerdo para linhas
         indice = y // tam_cel
         mover_linha(indice, 'esq')
-    elif x > tam_tela - tam_cel:  # Botão direito para linhas
+        mover_coluna(indice, 'bai')
+    elif x > tam_tela - larg_borda:  # Botão direito para linhas
         indice = y // tam_cel
         mover_linha(indice, 'dir')
-    elif y < tam_cel:  # Botão superior para colunas
+        mover_coluna(indice, 'cim')
+    elif y < larg_borda:  # Botão superior para colunas
         indice = x // tam_cel
         mover_coluna(indice, 'cim')
-    elif y > tam_tela - tam_cel:  # Botão inferior para colunas
+        mover_linha(indice, 'dir')
+    elif y > tam_tela - larg_borda:  # Botão inferior para colunas
         indice = x // tam_cel
         mover_coluna(indice, 'bai')
+        mover_linha(indice, 'esq')
 
         #fim chat
 
